09583b756b822b3636a21bd570b6cc86e835a53c
flix29 <flix.1895@web.de>
2025-09-14 19:47:09 +0200

    add SproutMethodSecurityConfiguration and properties for method security support

---
+++ sprout-runtime/src/main/java/de/flix29/sprout/runtime/security/SproutMethodSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package de.flix29.sprout.runtime.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@AutoConfiguration
+@ConditionalOnClass(EnableMethodSecurity.class)
+@EnableConfigurationProperties(SproutMethodSecurityProperties.class)
+@ConditionalOnProperty(prefix = "sprout.security.method-security", name = "enabled", havingValue = "true")
+@EnableMethodSecurity
+public class SproutMethodSecurityConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(SproutMethodSecurityConfiguration.class);
+
+    private SproutMethodSecurityConfiguration() {
+        log.info("[Sprout] Method Security enabled via sprout.security.method-security.enabled=true");
+    }
+}
