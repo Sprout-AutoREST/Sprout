cc87c06c5690af2b32962d0772de9814cc91d7b7
flix29 <flix.1895@web.de>
2025-09-19 18:34:43 +0200

    remove logging from SproutMethodSecurityConfiguration constructor

--- sprout-runtime/src/main/java/de/flix29/sprout/runtime/security/SproutMethodSecurityConfiguration.java
+++ sprout-runtime/src/main/java/de/flix29/sprout/runtime/security/SproutMethodSecurityConfiguration.java
@@ -1,7 +1,5 @@
 package de.flix29.sprout.runtime.security;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,9 +13,4 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableMethodSecurity
 public class SproutMethodSecurityConfiguration {
 
-    private static final Logger log = LoggerFactory.getLogger(SproutMethodSecurityConfiguration.class);
-
-    SproutMethodSecurityConfiguration() {
-        log.info("[Sprout] Method Security enabled via sprout.security.method-security.enabled=true");
-    }
 }
